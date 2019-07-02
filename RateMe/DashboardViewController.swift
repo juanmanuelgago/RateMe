2800b43413d11214c91713c4da010f591f4c9bec
Juan Manuel Gago <jmgago279@gmail.com>
2019-07-02 20:26:20 -0300

    add empty view when no users are shown in dashboard

--- RateMe/DashboardViewController.swift
+++ RateMe/DashboardViewController.swift
@@ -142,8 +142,27 @@ extension DashboardViewController: UICollectionViewDelegate, UICollectionViewDat
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if filtersFlag || searching {
+            if filteredUsers.count == 0 {
+                if activityIndicator.isAnimating {
+                    collectionView.setEmptyView(title: "Wait", message: "Getting users...")
+                } else {
+                    collectionView.setEmptyView(title: "Ups!", message: "No users matching the filters applied.")
+                }
+                
+            } else {
+                collectionView.restore()
+            }
             return filteredUsers.count
         } else {
+            if users.count == 0 {
+                if activityIndicator.isAnimating {
+                    collectionView.setEmptyView(title: "Wait", message: "Getting users...")
+                } else {
+                    collectionView.setEmptyView(title: "Ups!", message: "It seems you have no friends!")
+                }
+            } else {
+                collectionView.restore()
+            }
             return users.count
         }
     }
@@ -170,8 +189,6 @@ extension DashboardViewController: UICollectionViewDelegate, UICollectionViewDat
 
 extension DashboardViewController: UserReviewDelegate {
     func didRequestToReview(cell: UICollectionViewCell) {
-        // GET REVIEWS FOR THE GROUPS OF THE USER
-        // SHOW ACTION SHEET WITH OPTIONS
         if let cellLocation = userCollectionView.indexPath(for: cell) as IndexPath? {
             var userOfCell: User
             if filtersFlag || searching {
@@ -353,4 +370,38 @@ extension DashboardViewController: FilterDelegate {
     }
     
 }
+
+extension UICollectionView {
+    
+    func setEmptyView(title: String, message: String) {
+        let emptyView = UIView(frame: CGRect(x: self.center.x, y: self.center.y, width: self.bounds.size.width, height: self.bounds.size.height))
+        let titleLabel = UILabel()
+        let messageLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textColor = UIColor.black
+        titleLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 18)
+        messageLabel.textColor = UIColor.lightGray
+        messageLabel.font = UIFont(name: "HelveticaNeue-Regular", size: 17)
+        emptyView.addSubview(titleLabel)
+        emptyView.addSubview(messageLabel)
+        titleLabel.centerYAnchor.constraint(equalTo: emptyView.centerYAnchor).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: emptyView.centerXAnchor).isActive = true
+        messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
+        messageLabel.leftAnchor.constraint(equalTo: emptyView.leftAnchor, constant: 20).isActive = true
+        messageLabel.rightAnchor.constraint(equalTo: emptyView.rightAnchor, constant: -20).isActive = true
+        titleLabel.text = title
+        messageLabel.text = message
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        self.backgroundView = emptyView
+    }
+    
+    func restore() {
+        // Removes the view, if there's at least one purchase detected.
+        self.backgroundView = nil
+    }
+
+}
+
 
